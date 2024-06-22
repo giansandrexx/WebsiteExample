3ca73e774bd1b81294fb2b55cea2904dcb5e2ec9
Giyan <137732075+giansandrexx@users.noreply.github.com>
2024-06-22 18:44:35 +0800

    Add files via upload

--- Website/JS/Script.js
+++ Website/JS/Script.js
@@ -1,85 +1,148 @@
 document.addEventListener('DOMContentLoaded', function() {
     const shoesImage = document.getElementById('shoes-image');
     const toggleButtons = document.querySelectorAll('.round-toggle');
+    const aj1titleElement = document.querySelector('.aj1-sub');
+    const aj1aboutElement = document.querySelector('.about');
+    const shoeImageElement = document.getElementById('shoes-image');
+
+    const colorsLow = {
+        black: '../Shoes/AJ 1 Low Black.png',
+        iceblue: '../Shoes/AJ 1 Low Ice blue.png',
+        luckygreen: '../Shoes/AJ 1 Low Lucky Green.png',
+        wolfgrey: '../Shoes/AJ 1 Low Wolf Grey.png',
+        sportblue: '../Shoes/AJ 1 Low Sport Blue.png',
+        palevanilla: '../Shoes/AJ 1 Low Pale Vanilla.png',
+        sunset: '../Shoes/AJ 1 Low Sunset.png',
+        desert: '../Shoes/AJ 1 Low Desert.png',
+        jadesmoke: '../Shoes/AJ 1 Low Gym Red.png'
+    };
+
+    const colorsHigh = {
+        black: '../Shoes/AJ 1 High Black.png',
+        varsityred: '../Shoes/AJ 1 High Varsity Red.png',
+        latte: '../Shoes/AJ 1 High Latte.png',
+        sunrise: '../Shoes/AJ 1 High Sunrise.png',
+        bluesea: '../Shoes/AJ 1 High Blue Sea.png',
+        cyanite: '../Shoes/AJ 1 High Cyanite.png',
+    };
+
+    let currentColors = colorsLow;
 
     toggleButtons.forEach(button => {
         button.addEventListener('click', function() {
             const color = this.getAttribute('data-color');
-            let newImageSrc;
-
-            switch(color) {
-                case 'black':
-                    newImageSrc = '../Shoes/AJ 1 Low Black.png';
-                    break;
-                case 'iceblue':
-                    newImageSrc = '../Shoes/AJ 1 Low Ice blue.png';
-                    break;
-                case 'luckygreen':
-                    newImageSrc = '../Shoes/AJ 1 Low Lucky Green.png';
-                    break;
-                case 'wolfgrey':
-                    newImageSrc = '../Shoes/AJ 1 Low Wolf Grey.png';
-                    break;
-                case 'sportblue':
-                    newImageSrc = '../Shoes/AJ 1 Low Sport Blue.png';
-                    break;
-                case 'palevanilla':
-                    newImageSrc = '../Shoes/AJ 1 Low Pale Vanilla.png';
-                    break;
-                case 'sunset':
-                    newImageSrc = '../Shoes/AJ 1 Low Sunset.png';
-                    break;
-                case 'desert':
-                    newImageSrc = '../Shoes/AJ 1 Low Desert.png';
-                    break;
-                case 'jadesmoke':
-                    newImageSrc = '../Shoes/AJ 1 Low Jade Smoke.png';
-                    break;
+            const newImageSrc = currentColors[color];
+            if (newImageSrc) {
+                shoesImage.src = newImageSrc;
             }
-            shoesImage.src = newImageSrc;
         });
     });
 
-    const buttons = document.querySelectorAll('.round-toggle');
-    
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const color = button.getAttribute('data-color');
-            document.body.style.backgroundColor = color;
-        });
+    document.getElementById('next-button').addEventListener('click', function() {
+        aj1titleElement.classList.add('zoom-out');
+        aj1aboutElement.classList.add('zoom-out');
+        shoeImageElement.classList.add('zoom-out');
+
+        setTimeout(() => {
+            if (aj1aboutElement.innerHTML === 'The Air Jordan 1 High is not only a foundational piece of the Air Jordan <br>line but also a staple in the world of sneakers, known for its enduring style, <br>historical significance, and cultural impact.') {   
+                aj1aboutElement.innerHTML = 'The Air Jordan 1 Low is a classic and versatile sneaker that combines <br>the rich history of the Air Jordan brand with a contemporary, <br>low-top design suitable for everyday wear.';
+                aj1titleElement.innerHTML = 'LOW';
+                shoeImageElement.src = '../Shoes/AJ 1 Low Black.png';
+                currentColors = colorsLow;
+                updateToggleButtons(Object.keys(colorsLow));
+            } else {
+                aj1aboutElement.innerHTML = 'The Air Jordan 1 High is not only a foundational piece of the Air Jordan <br>line but also a staple in the world of sneakers, known for its enduring style, <br>historical significance, and cultural impact.';
+                aj1titleElement.innerHTML = 'HIGH';
+                shoeImageElement.src = '../Shoes/AJ 1 High Black.png'; 
+                currentColors = colorsHigh;
+                updateToggleButtons(Object.keys(colorsHigh));
+            }
+
+            aj1titleElement.classList.remove('zoom-out');
+            aj1aboutElement.classList.remove('zoom-out');
+            shoeImageElement.classList.remove('zoom-out');
+            
+            aj1titleElement.classList.add('zoom-in');
+            aj1aboutElement.classList.add('zoom-in');
+            shoeImageElement.classList.add('zoom-in');
+
+            setTimeout(() => {
+                aj1titleElement.classList.remove('zoom-in');
+                aj1aboutElement.classList.remove('zoom-in');
+                shoeImageElement.classList.remove('zoom-in');
+            }, 500);
+        }, 500);
     });
 
-    const navButtons = document.querySelectorAll('.nav-button');
-
-    navButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            navButtons.forEach(btn => btn.classList.remove('hovered'));
-            button.classList.add('hovered');
-        });
-    });
-
-    function clearContent() {
-        const elements = document.querySelectorAll('body > *:not(.just-do-it-banner)');
-        elements.forEach(element => {
-            element.style.display = 'none';
+    function updateToggleButtons(colors) {
+        toggleButtons.forEach((button, index) => {
+            if (index < colors.length) {
+                button.setAttribute('data-color', colors[index]);
+                button.style.background = getColorGradient(colors[index]);
+                button.style.display = 'flex';
+            } else {
+                button.style.display = 'none';
+            }
         });
     }
 
-    document.getElementById('next-button').addEventListener('click', function() {
-        const aj1titleElement = document.querySelector('.aj1-sub');
-        const aj1aboutElement = document.querySelector('.about');
-        const shoeImageElement = document.getElementById('shoes-image');
-        
-        if (aj1aboutElement.innerHTML === 'The Air Jordan 1 High is not only a foundational piece of the Air Jordan <br>line but also a staple in the world of sneakers, known for its enduring style, <br>historical significance, and cultural impact.') {   
-            aj1aboutElement.innerHTML = 'The Air Jordan 1 Low is a classic and versatile sneaker that combines <br>the rich history of the Air Jordan brand with a contemporary, <br>low-top design suitable for everyday wear.';
-            aj1titleElement.innerHTML = 'LOW';
-            shoeImageElement.src = '../Shoes/AJ 1 Low Black.png';
-        } else {
-            aj1aboutElement.innerHTML = 'The Air Jordan 1 High is not only a foundational piece of the Air Jordan <br>line but also a staple in the world of sneakers, known for its enduring style, <br>historical significance, and cultural impact.';
-            aj1titleElement.innerHTML = 'HIGH';
-            shoeImageElement.src = '../Shoes/AJ 1 High Black.png'; 
+    function getColorGradient(color) {
+        switch (color) {
+            case 'black':
+                return 'linear-gradient(to right, black 50%, white 50%)';
+            case 'iceblue':
+                return 'linear-gradient(to right, skyblue 50%, white 50%)';
+            case 'luckygreen':
+                return 'linear-gradient(to right, darkgreen 50%, white 50%)';
+            case 'wolfgrey':
+                return 'linear-gradient(to right, grey 50%, white 50%)';
+            case 'sportblue':
+                return 'linear-gradient(to right, hsl(240, 96%, 59%) 50%, white 50%)';
+            case 'palevanilla':
+                return 'linear-gradient(to right, hsl(48, 75%, 81%), 50%, white 50%)';
+            case 'sunset':
+                return 'linear-gradient(to right, hsl(10, 81%, 83%) 0%, hsl(10, 81%, 83%) 50%, white 50%, white 100%)';
+            case 'desert':
+                return 'linear-gradient(to right, black 50%, sandybrown 50%)';
+            case 'jadesmoke':
+                return 'linear-gradient(to right, black 50%, red 50%)';
+            case 'varsityred':
+                return 'linear-gradient(to right, #800000 50%, white 50%)';
+            case 'latte':
+                return 'linear-gradient(to right, #C4A484 50%, white 50%)';
+            case 'sunrise':
+                return 'linear-gradient(to right, #A52A2A 50%, white 50%)';
+            case 'bluesea':
+                return 'linear-gradient(to right, purple 50%, white 50%)';
+            case 'cyanite':
+                return 'linear-gradient(to right, #00FFFF 50%, white 50%)';
+            default:
+                return 'linear-gradient(to right, black 50%, white 50%)';
         }
-    });    
-});
+    }
 
+    updateToggleButtons(Object.keys(colorsLow));
+
+    function updateToggleButtons(colors) {
+        toggleButtons.forEach((button, index) => {
+            button.classList.add('fade-out');
     
+            setTimeout(() => {
+                if (index < colors.length) {
+                    button.setAttribute('data-color', colors[index]);
+                    button.style.background = getColorGradient(colors[index]);
+                    button.style.display = 'flex';
+                } else {
+                    button.style.display = 'none';
+                }
+    
+                button.classList.remove('fade-out');
+                button.classList.add('fade-in');
+                
+                setTimeout(() => {
+                    button.classList.remove('fade-in');
+                }, 500);
+            }, 500);
+        });
+    }
+});
